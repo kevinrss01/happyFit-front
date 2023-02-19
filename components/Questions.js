a2e646e154ac20a602f7b15b06f6409f471f9e23
kevinrss01 <kevin.rousseau3@gmail.com>
2023-02-19 15:33:53 +0100

    solving bug with useId()

--- components/Questions.js
+++ components/Questions.js
@@ -1,79 +1,91 @@
-import React, {useState, useCallback, useId} from "react";
+import React, { useState, useCallback, useId } from "react";
 
 const defaultQuestions = {
-    question1 : [
-        {text: "Débutant", selected: false},
-        {text: "Intermédiaire", selected: false},
-        {text: "Expert", selected: false},
-    ],
-question2 : [
-        {text: "Perte de poids", selected: false},
-        {text: "Prise de masse", selected: false},
-        {text: "Remise en forme", selected: false},
-],
-question3 : [
-        {text: "30 minutes", selected: false},
-        {text: "1 heure", selected: false},
-        {text: "1h30", selected: false},
-        {text: "2 heures", selected: false},
-],
-question4 : [
-        {text: "option 1", selected: false},
-        {text: "option 2", selected: false},
-        {text: "option 3", selected: false},
-        {text: "option 4", selected: false},
-
-],
-question5 : [
-        {text: "option 1", selected: false},
-        {text: "option 2", selected: false},
-],
-}
+  question1: [
+    { text: "Débutant", selected: false },
+    { text: "Intermédiaire", selected: false },
+    { text: "Expert", selected: false },
+  ],
+  question2: [
+    { text: "Perte de poids", selected: false },
+    { text: "Prise de masse", selected: false },
+    { text: "Remise en forme", selected: false },
+  ],
+  question3: [
+    { text: "30 minutes", selected: false },
+    { text: "1 heure", selected: false },
+    { text: "1h30", selected: false },
+    { text: "2 heures", selected: false },
+  ],
+  question4: [
+    { text: "option 1", selected: false },
+    { text: "option 2", selected: false },
+    { text: "option 3", selected: false },
+    { text: "option 4", selected: false },
+  ],
+  question5: [
+    { text: "option 1", selected: false },
+    { text: "option 2", selected: false },
+  ],
+};
 
 const selectedStyle = {
-    backgroundColor: "black",
-    color: "white"
-}
+  backgroundColor: "black",
+  color: "white",
+};
 
 const defaultStyle = {
-    backgroundColor: "inherit",
-    color: "inherit"
-}
+  backgroundColor: "inherit",
+  color: "inherit",
+};
 
 function Questions() {
-    const [questions, setQuestions] = useState(defaultQuestions);
+  const [questions, setQuestions] = useState(defaultQuestions);
 
-    const handleClick = useCallback((question, value) => {
-        setQuestions(prevQuestions => ({
-            ...prevQuestions,
-            [question] : prevQuestions[question].map(({text, selected}) => ({
-                text,
-                selected: !selected && text === value,
-            }))
-        }))
-    }, []);
+  const handleClick = useCallback((question, value) => {
+    setQuestions((prevQuestions) => ({
+      ...prevQuestions,
+      [question]: prevQuestions[question].map(({ text, selected }) => ({
+        text,
+        selected: !selected && text === value,
+      })),
+    }));
+  }, []);
 
-const validate = useCallback(() => {
-    const selectedAnswers = Object.keys(questions).map(key => questions[key].find(value => value.selected));
-    if(!selectedAnswers.includes(undefined)) {
-        console.log(selectedAnswers.map(({text})=> text));
-        setQuestions(defaultQuestions);
+  const validate = useCallback(() => {
+    const selectedAnswers = Object.keys(questions).map((key) =>
+      questions[key].find((value) => value.selected)
+    );
+    if (!selectedAnswers.includes(undefined)) {
+      console.log(selectedAnswers.map(({ text }) => text));
+      setQuestions(defaultQuestions);
     }
-}, [questions])
+  }, [questions]);
+
+  const keyId = useId();
+  const keyIdBis = useId();
 
   return (
     <div>
-        {
-            Object.keys(questions).map(key =>{
-                const keyId=useId();
-                return <div key={keyId}>
+      {Object.keys(questions).map((key) => {
+        return (
+          <div key={keyId}>
             <h2>{key}</h2>
-            {questions[key].map(({text, selected}) => {
-                const keyIdBis = useId();
-                return <button key={keyIdBis} onClick={() => handleClick(key, text)} style={selected ? selectedStyle : defaultStyle}>{text}</button>})}
-            </div>})
-        }
-        <button onClick={validate}>Continuer</button>
+            {questions[key].map(({ text, selected }) => {
+              return (
+                <button
+                  key={keyIdBis}
+                  onClick={() => handleClick(key, text)}
+                  style={selected ? selectedStyle : defaultStyle}
+                >
+                  {text}
+                </button>
+              );
+            })}
+          </div>
+        );
+      })}
+      <button onClick={validate}>Continuer</button>
     </div>
   );
 }
