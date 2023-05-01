3e57d5031fda9d27eb53805de3932f9477f53cd7
kevinrss01 <kevin.rousseau3@gmail.com>
2023-05-01 14:54:49 +0200

    updates packages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,5 @@
-import Head from "next/head";
-import Image from "next/image";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getNutrition } from "../redux/actions/nutritionActions";
-import styles from "../styles/Home.module.css";
-import Axios from "../service/axios";
-import ProgramAPI from "../service/ProgramAPI";
 import { getProgram } from "../redux/actions/sportActions";
 
 export default function Home() {
