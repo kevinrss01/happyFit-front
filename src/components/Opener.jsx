1f141cd5f917f9480dd817b58e702e46ee4229a6
kevinrss01 <kevin.rousseau3@gmail.com>
2023-08-19 17:32:53 +0200

    Progress style sport program page

--- src/components/Opener.jsx
+++ src/components/Opener.jsx
@@ -1,47 +1,51 @@
 import { useCallback, useMemo, useState } from 'react'
+import { Card } from '@tremor/react'
 
 const Opener = ({ message, children }) => {
-  const [isOpened, setIsOpened] = useState(false)
+   const [isOpened, setIsOpened] = useState(false)
 
-  const handleClick = useCallback(() => {
-    setIsOpened((prev) => !prev)
-  }, [])
+   const handleClick = useCallback(() => {
+      setIsOpened((prev) => !prev)
+   }, [])
 
-  const icone = useMemo(
-    () => <i className={`fa fa-angle-${isOpened ? 'up' : 'down'}`} style={{ marginRight: 5 }}></i>,
-    [isOpened],
-  )
+   const icone = useMemo(
+      () => (
+         <i className={`fa fa-angle-${isOpened ? 'up' : 'down'}`} style={{ marginRight: 5 }}></i>
+      ),
+      [isOpened],
+   )
 
-  return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-        gap: 10,
-      }}
-    >
-      <button className='button-opener' onClick={handleClick}>
-        {icone}
-        {message}
-      </button>
-      {isOpened && (
-        <div
-          style={{
+   //Ajouter le fond dégradé pour la Card
+
+   return (
+      <Card
+         style={{
             display: 'flex',
+            justifyContent: 'center',
             alignItems: 'center',
             flexDirection: 'column',
-            gap: 5,
-            marginBottom: 10,
-            width: 200,
-          }}
-        >
-          {children}
-        </div>
-      )}
-    </div>
-  )
+            gap: 10,
+            width: '60%',
+         }}
+      >
+         <button className='button-opener' onClick={handleClick}>
+            {icone}
+            {message}
+         </button>
+         <div
+            style={{
+               display: 'flex',
+               alignItems: 'center',
+               flexDirection: 'column',
+               gap: 5,
+               marginBottom: 10,
+               width: 200,
+            }}
+         >
+            {children}
+         </div>
+      </Card>
+   )
 }
 
 export default Opener
