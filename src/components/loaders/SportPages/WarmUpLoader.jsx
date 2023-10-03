8fed518280277bd30ed52278bb13605d145fb37d
kevinrss01 <kevin.rousseau@protonmail.com>
2023-10-03 18:51:19 +0200

    added width handler for warmup loader

--- src/components/loaders/SportPages/WarmUpLoader.jsx
+++ src/components/loaders/SportPages/WarmUpLoader.jsx
@@ -1,24 +1,48 @@
 import Skeleton from 'react-loading-skeleton'
+import { useEffect, useState } from 'react'
 
-const WarmUpLoader = (props) => (
-   <>
-      <Skeleton height={40} width={500} count={1} className='mt-10 mb-5' />
-      <Skeleton height={35} width={300} count={1} className='mt-5 mb-5' />
-      <Skeleton count={1} height={50} width={700} />
-      <Skeleton count={1} height={150} width={700} />
-      <Skeleton count={1} height={50} width={700} />
+const WarmUpLoader = () => {
+   const [windowWidth, setWindowWidth] = useState(window.innerWidth)
 
-      <div className='flex items-center justify-between'>
-         <div className='m-5 flex flex-col items-center justify-center'>
-            <Skeleton height={25} width={250} className='mt-5 mb-5' />
-            <Skeleton height={250} width={300} />
+   useEffect(() => {
+      // Fonction de rappel pour mettre à jour la largeur de la page
+      const handleResize = () => {
+         setWindowWidth(window.innerWidth)
+      }
+
+      // Ajouter un écouteur d'événement pour la taille de la fenêtre
+      window.addEventListener('resize', handleResize)
+
+      // Nettoyer l'écouteur d'événement lors du démontage du composant
+      return () => {
+         window.removeEventListener('resize', handleResize)
+      }
+   }, [])
+
+   const getWidth = (percentage) => {
+      return (windowWidth * percentage) / 100
+   }
+
+   return (
+      <>
+         <Skeleton height={40} width={getWidth(50)} count={1} className='mt-10 mb-5' />
+         <Skeleton height={35} width={getWidth(25)} count={1} className='mt-5 mb-5' />
+         <Skeleton count={1} height={50} width={getWidth(47)} />
+         <Skeleton count={1} height={150} width={getWidth(47)} />
+         <Skeleton count={1} height={50} width={getWidth(47)} />
+
+         <div className='flex items-center justify-between'>
+            <div className='m-5 flex flex-col items-center justify-center'>
+               <Skeleton height={25} width={getWidth(15)} className='mt-5 mb-5' />
+               <Skeleton height={250} width={getWidth(20)} />
+            </div>
+            <div className='m-5 flex flex-col items-center'>
+               <Skeleton height={25} width={getWidth(15)} className='mt-5 mb-5' />
+               <Skeleton height={250} width={getWidth(20)} />
+            </div>
          </div>
-         <div className='m-5 flex flex-col items-center'>
-            <Skeleton height={25} width={250} className='mt-5 mb-5' />
-            <Skeleton height={250} width={300} />
-         </div>
-      </div>
-   </>
-)
+      </>
+   )
+}
 
 export default WarmUpLoader
